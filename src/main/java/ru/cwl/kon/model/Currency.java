c2415fda9ed68066164428961a3bb5bedf9b2101
Vadim Tishenko <vad.tishenko@gmail.com>
2015-03-19 13:16:00 +0300

    sec commit

---
+++ src/main/java/ru/cwl/kon/model/Currency.java
@@ -0,0 +1,7 @@
+package ru.cwl.kon.model;
+
+/**
+ * Created by vad on 19.03.2015 4:14.
+ */
+public class Currency {
+}
---
+++ src/main/java/ru/cwl/kon/model/User.java
@@ -0,0 +1,7 @@
+package ru.cwl.kon.model;
+
+/**
+ * Created by vad on 19.03.2015 4:11.
+ */
+public class User {
+}
