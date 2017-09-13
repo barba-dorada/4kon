7d693ce7ef6031bdb3f85ae135af2555dd4f86c7
tishenko <vad.tishenko@gmail.com>
2017-09-13 19:25:18 +0300

    add vad

--- client-bank-import/src/main/java/app/Quickstart.java
+++ client-bank-import/src/main/java/app/Quickstart.java
@@ -35,40 +35,66 @@ public class Quickstart {
         List<List<Object>> values = s.readRange("факты!A2:H");//response.getValues();
         List<GHRow> factsList = readAndExtractFacts(values);
 
+        //testLera(factsList);
+        testVad(factsList, Month.AUGUST);
 
-/*
-        for (GHRow row : factsList) {
-            System.out.println(row);
-        }*/
 
-        // Лера тинькоф август
-        Month month = Month.AUGUST;
+    }
+
+    private static void testVad(List<GHRow> factsList, Month month) throws IOException {
+
         List<GHRow> rrr = factsList.stream()
-                .filter(r -> r.acc.equalsIgnoreCase("л.тинькоф"))
+                .filter(r -> r.acc.equalsIgnoreCase("в.тинькоф"))
                 .filter(r -> r.date.getMonth() == month)
                 .collect(toList());
 
-        for (GHRow row : rrr) {
-            System.out.println(row);
-        }
-
-        String fileName = "/csv/l/operations Tue Mar 14 15_33_28 MSK 2017-Fri Sep 08 19_18_45 MSK 2017.csv";
-        final Reader reader = new InputStreamReader(Quickstart.class.getResourceAsStream(fileName), "windows-1251");
-
-        CsvReader csvReader = new CsvReader();
-        List<Row> res = csvReader.read(reader);
+        List<Row> res = getRowsFromCsv("/csv/v/operations Fri Dec 09 00_00_00 MSK 2016-Sat Sep 09 22_00_51 MSK 2017.csv");
 
         List<Row> res2 = res.stream()
                 .filter(r -> r.getOperationDate().getMonth() == month)
                 .filter(r->r.getStatus().equals("OK"))
+                .filter(r->r.getCardNumber().equals("*9819") || r.getCardNumber().equals(""))
                 .collect(toList());
 
-        for (Row re : res2) {
-            System.out.println(re);
-        }
-
-        ArrayList<GHRow> a = new ArrayList(rrr);
+        ArrayList<GHRow> a = new ArrayList<GHRow>(rrr);
         ArrayList<Row> b = new ArrayList<Row>(res2);
+
+        merge(a, b);
+
+        BigDecimal c1 = print(b);
+        BigDecimal c2 = print2(a);
+        System.out.printf("c1-c2: %s",c1.subtract(c2));
+
+    }
+
+    private static BigDecimal print2(ArrayList<GHRow> a) {
+        // TODO: 13.09.2017 добавить печать заготовков
+        // TODO: 13.09.2017 Заменить 2 процедуры одной
+        System.out.println("========================");
+        BigDecimal c2=BigDecimal.ZERO;
+
+        for (GHRow row : a) {
+            System.out.println(row);
+            c2=c2.add(row.sum);
+
+        }
+        System.out.println(c2);
+        return c2;
+    }
+
+    private static BigDecimal print(ArrayList<Row> b) {
+        System.out.println("========================");
+        BigDecimal c1=BigDecimal.ZERO;
+        for (Row row : b) {
+            System.out.println(row);
+            c1=c1.add(row.getSum());
+        }
+        System.out.println(c1);
+        return c1;
+    }
+
+    private static void merge(ArrayList<GHRow> a, ArrayList<Row> b) {
+
 
 
         Iterator<Row> i = b.iterator();
@@ -86,25 +112,29 @@ public class Quickstart {
             }
         }
 
-        System.out.println("========================");
-        BigDecimal c1=BigDecimal.ZERO;
-        for (Row row : b) {
-            System.out.println(row);
-            c1=c1.add(row.getSum());
-        }
-        System.out.println(c1);
+
+ //       "обед","Рестораны","J HOUSE LTD";
+//       "перевод" "Наличные"
 
 
-        System.out.println("========================");
-        BigDecimal c2=BigDecimal.ZERO;
+    }
 
-        for (GHRow row : a) {
-            System.out.println(row);
-            c2=c2.add(row.sum);
+    private static List<Row> getRowsFromCsv(String fileName) throws IOException {
+        final Reader reader = new InputStreamReader(Quickstart.class.getResourceAsStream(fileName), "windows-1251");
 
-        }
-        System.out.println(c2);
-        System.out.printf("c1-c2: %s",c1.subtract(c2));
+        CsvReader csvReader = new CsvReader();
+        return csvReader.read(reader);
+    }
+
+    private static void testLera(List<GHRow> factsList) throws IOException {
+    /*
+            for (GHRow row : factsList) {
+                System.out.println(row);
+            }*/
+
+        // Лера тинькоф август
+        Month month = Month.AUGUST;
+        testVad(factsList, month);
 
 
        /* values = s.readRange("'остатки'!B2:C13");
@@ -116,6 +146,7 @@ public class Quickstart {
             n++;
         }*/
     }
+
 
     private static List<GHRow> readAndExtractFacts(List<List<Object>> values) {
 
